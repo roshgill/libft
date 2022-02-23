cf7fb1a5de04d417dfa5a94d8f218427d6dcfd3b
roshgill <99330131+roshgill@users.noreply.github.com>
2022-02-23 17:12:07 -0500

    First Commit

---
+++ libft/ft_strchr.c
@@ -0,0 +1,37 @@
+/*                                                        :::      ::::::::   */
+/*                                         			   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosgill <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 15:43:57 by rosgill           #+#    #+#             */
+/*   Updated: 2021/11/10 15:59:51 by rosgill          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* The C library function char *strrchr(const char *str, int c) 
+searches for the last occurrence of the character c (an unsigned char) 
+in the string pointed to, by the argument str. */
+
+char *ft_strchr(char *str, int c)
+{
+    int i = ft_strlen(str);
+
+    while (i >= 0)
+    {
+        if (str[i] == c)
+        {
+            char *point_to_location = &str[i];
+            return (point_to_location);
+        }
+        i--;
+    }
+    return(NULL);
+}
+
+// int main(void)
+// {
+//     char *location = ft_strchr("Roshaniscool",'i');
+//     return(0);
+// }
